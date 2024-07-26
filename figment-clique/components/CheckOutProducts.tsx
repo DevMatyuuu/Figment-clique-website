aaee8613a53f436c9107d8418bd0bd1dd6e711ea
Ross Matthew Santos <matthewsantos120216@gmail.com>
2024-07-26 18:52:39 +0800

    Update the layout of the checkout products

--- figment-clique/components/CheckOutProducts.tsx
+++ figment-clique/components/CheckOutProducts.tsx
@@ -10,10 +10,13 @@ export default function CheckOutProducts() {
     <div className='w-full pt-10'>
       {cart.map((item) => (
         <div className='flex w-full justify-between items-center'>
-          <div className='flex items-center justify-between w-full'>
-            <CldImage src={item.image} alt={item.title} width={100} height={80}/>
-            <span className='text-sm'>{item.title}</span>
-            <span>₱{item.price}</span>
+          <div className='flex justify-between w-full'>
+            <div className='flex relative gap-10'>
+              <CldImage src={item.image} alt={item.title} width={100} height={80}/>
+              <span className='text-sm mt-8 w-36'>{item.title}</span>
+              <span className='absolute top-4 left-20 bg-black text-white h-max w-5 text-center text-sm rounded-full'>{item.quantity}</span>
+            </div>
+            <span className='text-sm mt-8'>₱{item.price * item.quantity}</span>
           </div>
         </div>
       ))}
